2a4ea01f512dc74dee6fcffad94adde76f0806ca
napzalav <cesaralavila@hotmail.com>
2023-10-27 16:10:14 -0300

    add html&js para boton de formulario

--- Challenge_002-Formulario Dinámico para Tarjeta de Credito/main.js
+++ Challenge_002-Formulario Dinámico para Tarjeta de Credito/main.js
@@ -1,7 +1,13 @@
 // en esta variable vamos a guardar nuestra tarjeta y acceder a ella
-const tarjeta = document.querySelector('#tarjeta');
+const tarjeta = document.querySelector('#tarjeta'),
+    btnAbrirFormulario = document.querySelector('#btn-abrir-formulario');
 
-// * Rotacion de la tarjeta
+// Rotacion de la tarjeta
 tarjeta.addEventListener('click', () => {
-	tarjeta.classList.toggle('active');
+    tarjeta.classList.toggle('active');
 });
+
+// Rotacion de boton
+btnAbrirFormulario.addEventListener('click', () => {
+	btnAbrirFormulario.classList.toggle('active');
+})
