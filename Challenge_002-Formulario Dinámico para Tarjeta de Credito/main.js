850f4a1db894c9eab6ff2139e461cb1a09d7e3b6
napzalav <cesaralavila@hotmail.com>
2023-10-29 03:47:16 -0300

    update selectbox Expiracion generado dinamicamente

--- Challenge_002-Formulario Dinámico para Tarjeta de Credito/main.js
+++ Challenge_002-Formulario Dinámico para Tarjeta de Credito/main.js
@@ -8,9 +8,27 @@ tarjeta.addEventListener('click', () => {
     tarjeta.classList.toggle('active');
 });
 
-// Rotacion de boton
+// Boton de abrir formulario / Formulario
 btnAbrirFormulario.addEventListener('click', () => {
 	btnAbrirFormulario.classList.toggle('active');
     formulario.classList.toggle('active');
 });
 
+// Select del mes generado dinamicamente
+for(let i = 1; i <= 12; i++){
+    // console.log(i);
+    let opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.innerText = i;
+    formulario.selectMes.appendChild(opcion);
+}
+
+// Select del año generado dinamicamente
+const yearActual = new Date().getFullYear();
+for(let i = yearActual; i <= yearActual + 8; i++){
+    // console.log(i);
+    let opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.innerText = i;
+    formulario.selectYear.appendChild(opcion);
+}
